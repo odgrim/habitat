3cc759093f9b8b4c32ac44caea39f46fcafd34e6
odgrim <brian@vfxectropy.com>
2013-03-25 22:37:13 -0700

    whitespace change and rename functions for wrapping

--- cuddles_core/cuddles.sh
+++ cuddles_core/cuddles.sh
@@ -1,50 +1,54 @@
 #!/usr/bin/env bash
 
+cuddles () {
+
+}
+
 # wrapper for loading dirs or files into our env
-load_modules() {
+__cuddles_load_modules() {
   # loop through args and hand off to helper if a dir or file
   while [[ $# -gt 0 ]]; do
     local token="$1" ; shift
 
     if [[ -d "$token" ]]; then
-		load_module_dir "$token"
-	elif [[ -f "$token" ]]; then
-		load_module_file "$token"
-	else 
-		echo "module $token not found"
-	fi
+    load_module_dir "$token"
+  elif [[ -f "$token" ]]; then
+    load_module_file "$token"
+  else 
+    echo "module $token not found"
+  fi
   done
 }
 
 # conveniance wrapper...
-load_module() {
-	load_modules $@
+__cuddles_load_module() {
+  load_modules $@
 }
 
 # load a dirs contents into the current env
-load_module_dir() {
-	load_path="$1"
+__cuddles_load_module_dir() {
+  load_path="$1"
 
-	init_path="${load_path}/init.*"
+  init_path="${load_path}/init.*"
 
-	if [ -e $init_path ] && [ -f $init_path ]; then
-		load_module_file $init_path
-	else
-		echo "module $load_path can not load. Did you forget an init.sh?"
+  if [ -e $init_path ] && [ -f $init_path ]; then
+    load_module_file $init_path
+  else
+    echo "module $load_path can not load. Did you forget an init.sh?"
     fi
 }
 
 # load target file into the current env
-load_module_file() { 
-	load_path="$1"
-	if [[ ! -f "$load_path" ]]; then
-		echo "error loading $load_path, does not exist!"
-		return 1
-	elif [[ ! -x "$load_path" ]]; then
-		echo "error loading $load_path, make sure it's executable!"
-		return 1
-	else
-		# it loads fine, so bring it in!
-		. "$load_path"
-	fi
+__cuddles_load_module_file() { 
+  load_path="$1"
+  if [[ ! -f "$load_path" ]]; then
+    echo "error loading $load_path, does not exist!"
+    return 1
+  elif [[ ! -x "$load_path" ]]; then
+    echo "error loading $load_path, make sure it's executable!"
+    return 1
+  else
+    # it loads fine, so bring it in!
+    . "$load_path"
+  fi
 }

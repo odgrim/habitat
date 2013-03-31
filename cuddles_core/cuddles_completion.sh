8524b6ff5596e9a1963725721dc8928fa4bbf78d
odgrim <brian@vfxectropy.com>
2013-03-30 22:53:43 -0700

    convert from loop spam to glob and array substitution

--- cuddles_core/cuddles_completion.sh
+++ cuddles_core/cuddles_completion.sh
@@ -3,41 +3,31 @@ __cuddles_completion ()
   local cur prev
   # Pointer to current completion word.
   # By convention, it's named "cur" but this isn't strictly necessary.
+  
   CUDDLES_DIR=$HOME/cuddles
   COMPREPLY=()   # Array variable storing the possible completions.
+  
   cur=${COMP_WORDS[COMP_CWORD]}
   prev=${COMP_WORDS[COMP_CWORD-1]}
+  
   shopt -s extglob nullglob
+  
   case "$prev" in
     cuddles)
       COMPREPLY=( $( compgen -W 'load' -- $cur ) )
       return 0      
     ;;
     load)
-      local full_module_paths=( ~/cuddles/${cur}*/init.sh )
-
-      for x in ${full_module_paths[@]}; do 
-        # get rid of the path leading to our cuddles location
-        local this_path=${x/$CUDDLES_DIR\/}
-
-        # and trim off the init.sh
-        local this_path=${this_path/\/init.sh}
-        
-        COMPREPLY+=( $this_path )
-      done
-      # since we're looking for dirs with init.sh in it,
-      # we can either use the -o of dirnames or filenames on complete
-      # if we use dirnames then the auto completion adds a space to the end of
-      # our arg since it finds a dir match in our full_module_paths glob.
-      # Here we force the file path to not complete further
-      if [[ ${#full_module_paths[@]} -eq 1 ]]; then {
-          COMPREPLY=( $this_path ) 
-      }
-      fi
-      shopt -u extglob nullglob
+      # glob to grab everything with an init.sh
+      COMPREPLY=( ~/cuddles/${cur}*/init.sh )
+      # Trim the ~/.cuddles that prepends the path
+      COMPREPLY=("${COMPREPLY[@]#"$CUDDLES_DIR"/}")
+      # and then get rid of the init.sh to leave just a module name
+      COMPREPLY=("${COMPREPLY[@]%/init.sh}")      
     ;;
   esac
-
+  
+  shopt -u extglob nullglob
   return 0
 }
 

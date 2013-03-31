f85662d095b884c173efe3cc7f02e5a4db2d52fe
odgrim <brian@vfxectropy.com>
2013-03-30 18:10:58 -0700

    removed final init.sh from completion with nullglob

--- cuddles_core/cuddles_completion.sh
+++ cuddles_core/cuddles_completion.sh
@@ -7,10 +7,15 @@ __cuddles_completion ()
   COMPREPLY=()   # Array variable storing the possible completions.
   cur=${COMP_WORDS[COMP_CWORD]}
   prev=${COMP_WORDS[COMP_CWORD-1]}
-
+  shopt -s extglob nullglob
   case "$prev" in
+    cuddles)
+      COMPREPLY=( $( compgen -W 'load' -- $cur ) )
+      return 0      
+    ;;
     load)
-      local full_module_paths=( ~/cuddles/$cur*/init.sh )
+      local full_module_paths=( ~/cuddles/${cur}*/init.sh )
+
       for x in ${full_module_paths[@]}; do 
         # get rid of the path leading to our cuddles location
         local this_path=${x/$CUDDLES_DIR\/}
@@ -20,11 +25,20 @@ __cuddles_completion ()
         
         COMPREPLY+=( $this_path )
       done
-      return 0
+      # since we're looking for dirs with init.sh in it,
+      # we can either use the -o of dirnames or filenames on complete
+      # if we use dirnames then the auto completion adds a space to the end of
+      # our arg since it finds a dir match in our full_module_paths glob.
+      # Here we force the file path to not complete further
+      if [[ ${#full_module_paths[@]} -eq 1 ]]; then {
+          COMPREPLY=( $this_path ) 
+      }
+      fi
+      shopt -u extglob nullglob
     ;;
   esac
-  COMPREPLY=( $( compgen -W 'load' -- $cur ) )
+
   return 0
 }
 
-complete -F __cuddles_completion cuddles
+complete -o filenames -F __cuddles_completion cuddles

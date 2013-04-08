9c4b5a74834a34580651916487dae1d00c8e363c
odgrim <brian@vfxectropy.com>
2013-04-07 20:43:53 -0700

    start blocking out core functionality of env_manipulation

--- lib/env_manipulation.sh
+++ lib/env_manipulation.sh
@@ -1,8 +1,31 @@
+#!/usr/bin/env bash
 is_in_var(){
-#[--partial --delineator ":"] VAR_NAME value
+	if [[ "$1" == "--partial" && "${!2}" == *${3}* ]]; then
+		return
+	else
+		return 1
+
 	if [[ "${!1}" == ${2} || "${!1}" == ${2}:* || "${!1}" == *:${2}:* || "${!1}" == *:${2} ]]; then
 		return
 	else
 		return 1
 	fi
 } 
+
+prepend_var(){
+	if [[ -z "${1}" ]]; then
+		export ${!1}=$2
+	fi
+}
+
+append_var(){
+
+}
+
+remove_from_var(){
+
+}
+
+replace_in_var(){
+
+}

222cca33d1041fcbe97845eab79299520b926403
odgrim <brian@vfxectropy.com>
2013-04-20 16:16:19 -0700

    unittest file added

---
+++ lib/unittest.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# 
+# test_suite- don't care about the name, just make it a file representing a feature to test
+### Core
+# run_all_tests- goes and gathers suites,queuestests, execs each. 
+# queue_test- Does the savin' bit, make sure we don't dupe up on scopies in the env var
+# run_test- kicks off a single test run, 
+# print_result- 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+run_all_tests(){
+
+}
+
+run_test(){
+
+}
+
